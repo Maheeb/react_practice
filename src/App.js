5edf8bf7da24217224b34631474ba16d149f8912
Azmaeen <maheeb009@gmail.com>
2022-09-06 00:53:18 +0600

    create first component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import ExpenseItem from "./components/ExpenseItem";
  const own_function = ()=>{
      return(
          <div>
              <h1 style={{textAlign:"center", color:"green"}}>hello,I am  Maheeb Mohammad Azmaeen</h1>
+             <ExpenseItem></ExpenseItem>
          </div>
      )
  }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,7 @@
+const expenseItem=()=>{
+    return(
+        <p style={{textAlign:"center",color:"red"}}>I am ExpenseItem component</p>
+    )
+}
+
+export default expenseItem;
