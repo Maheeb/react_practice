b9e6c6bf27269b441eda346c7a17d738488e3bef
Azmaeen <maheeb009@gmail.com>
2022-09-09 17:16:19 +0600

    Components and essentials css added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,17 @@
 import  "./ExpenseItem.css";
+import  ExpenseDate from "./ExpenseDate";
 const expenseItem=(props)=>{
+
+
 
     return(
       <div>
-          <div className="expense-item">{props.date.toISOString()}</div>
-          <div>
-              <h2 className="expense-item_description">{props.title}</h2>
-              <div className="expense-item_price">{props.price}$</div>
+          <div className='expense-item'>
+          <ExpenseDate date={props.date}> </ExpenseDate>
+          <div className='expense-item__description'>
+              <h2 className="">{props.title}</h2>
+              <div className='expense-item__price'>{props.price}$</div>
+          </div>
           </div>
       </div>
     )
